2cbea8576330d603b4e3d223b3e729d2128f8e22
ottoyoon98 <ottoyoon98@gmail.com>
2022-08-01 20:55:36 +0900

    add: temp images domains

--- zzal-app/next.config.js
+++ zzal-app/next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: [
+      'i.pinimg.com',
+      'snaptime.edaily.co.kr',
+      'blog.kakaocdn.net',
+      'pbs.twimg.com',
+      'mblogthumb-phinf.pstatic.net',
+      'pbs.twimg.com',
+    ],
+  }
 }
 
 module.exports = nextConfig
